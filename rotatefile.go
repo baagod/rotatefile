998624e50df46292cddb947970e03fd5cf3aca65
黄江桂 <780810441@qq.com>
2024-07-19 12:12:15 +0800

    在 New() 的同时判断是否需要轮转，并打开文件。

--- rotatefile.go
+++ rotatefile.go
@@ -25,52 +25,80 @@ type Writer struct {
 	file           *os.File
 	filepath       string
 	rotateInterval RotateInterval
-	rotateNext     time.Time
+	rotateAt       time.Time
 	mu             sync.Mutex
 }
 
-func New(filepath string, rotateInterval RotateInterval) (w *Writer, err error) {
-	err = os.MkdirAll(path.Dir(filepath), DefaultFilePerm)
-	if err == nil || os.IsExist(err) {
-		w = &Writer{filepath: filepath, rotateInterval: rotateInterval}
-	}
-	return
-}
-
-func (f *Writer) doRotate() (err error) {
-	if now := time.Now(); now.After(f.rotateNext) { // 需要轮转
-		if f.file != nil {
-			_ = f.file.Close() // 关闭打开的文件
-			oldpath := f.file.Name()
-			newpath := oldpath + "." + now.Format("20060102150405")
-			if err = os.Rename(oldpath, newpath); err != nil { // 重命名文件
-				return
-			}
-		}
-
-		y, m, d := now.Year(), now.Month(), now.Day()
-		switch f.rotateInterval {
-		case PerDay:
-			f.rotateNext = time.Date(y, m, d+1, 0, 0, 0, 0, time.Local)
-		case PerHour:
-			f.rotateNext = time.Date(y, m, d, now.Hour()+1, 0, 0, 0, time.Local)
-		case PerMinute:
-			f.rotateNext = time.Date(y, m, d, now.Hour(), now.Minute()+1, 0, 0, time.Local)
-		}
-
-		f.file, err = os.OpenFile(f.filepath, DefaultFileFlags, DefaultFilePerm)
-	}
-
-	return
-}
-
-func (f *Writer) Write(b []byte) (n int, err error) {
-	f.mu.Lock()
-	defer f.mu.Unlock()
-
-	if err = f.doRotate(); err != nil {
+func New(filepath string, rotate RotateInterval) (w *Writer, err error) {
+	err = os.MkdirAll(path.Dir(filepath), DefaultFilePerm) // 创建文件目录
+	if err != nil && !os.IsExist(err) {
 		return
 	}
 
-	return f.file.Write(b)
+	stat, err := os.Stat(filepath) // 获取文件信息
+	if err != nil && !os.IsNotExist(err) {
+		return nil, err
+	}
+
+	now := time.Now()
+	w = &Writer{filepath: filepath, rotateInterval: rotate}
+
+	if stat != nil {
+		// 判断文件是否需要现在轮转
+		if w.rotateAt = rotate.next(stat.ModTime()); now.After(w.rotateAt) {
+			if err = w.rename(now); err != nil { // 归档文件
+				return
+			}
+		}
+	}
+
+	w.file, err = os.OpenFile(w.filepath, DefaultFileFlags, DefaultFilePerm)
+	if err == nil {
+		w.rotateAt = rotate.next(now) // 设置下次轮转时间
+	}
+
+	return
+}
+
+func (w *Writer) doRotate() (err error) {
+	if now := time.Now(); now.After(w.rotateAt) { // 需要轮转
+		if err = w.rename(now); err != nil {
+			return
+		}
+		w.rotateAt = w.rotateAt.Add(time.Duration(w.rotateInterval) * time.Second)
+		w.file, err = os.OpenFile(w.filepath, DefaultFileFlags, DefaultFilePerm)
+	}
+
+	return
+}
+
+func (w *Writer) Write(b []byte) (n int, err error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if err = w.doRotate(); err != nil {
+		return
+	}
+	return w.file.Write(b)
+}
+
+func (w *Writer) rename(now time.Time) error {
+	if w.file != nil {
+		_ = w.file.Close()
+	}
+	oldpath := w.filepath
+	newpath := oldpath + "." + now.Format("20060102150405")
+	return os.Rename(oldpath, newpath)
+}
+
+func (interval RotateInterval) next(now time.Time) time.Time {
+	y, m, d := now.Year(), now.Month(), now.Day()
+	switch interval {
+	case PerDay:
+		return time.Date(y, m, d+1, 0, 0, 0, 0, time.Local)
+	case PerHour:
+		return time.Date(y, m, d, now.Hour()+1, 0, 0, 0, time.Local)
+	case PerMinute:
+		return time.Date(y, m, d, now.Hour(), now.Minute()+1, 0, 0, time.Local)
+	}
+	return time.Date(y, m, d+1, 0, 0, 0, 0, time.Local)
 }
